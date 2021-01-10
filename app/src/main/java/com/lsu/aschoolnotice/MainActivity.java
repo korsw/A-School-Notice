277562b6182c1784a71cfc798fe5f47741847b2f
korsw <tkddns5158@gmail.com>
2021-01-11 01:51:23 +0900

    Splash Activite

--- app/src/main/java/com/lsu/aschoolnotice/MainActivity.java
+++ app/src/main/java/com/lsu/aschoolnotice/MainActivity.java
@@ -3,6 +3,8 @@ package com.lsu.aschoolnotice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startLoading();
+    }
+
+    private void startLoading(){
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(getApplicationContext(), ShowNoticeBoard.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
     }
 }
