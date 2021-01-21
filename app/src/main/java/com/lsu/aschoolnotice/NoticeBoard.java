f20d17560dea6f9c4759ff0d88f49360479484c9
korsw <tkddns5158@gmail.com>
2021-01-22 00:11:32 +0900

    nav_bar 0.0.3

--- app/src/main/java/com/lsu/aschoolnotice/NoticeBoard.java
+++ app/src/main/java/com/lsu/aschoolnotice/NoticeBoard.java
@@ -2,6 +2,7 @@ package com.lsu.aschoolnotice;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
